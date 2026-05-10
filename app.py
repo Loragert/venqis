8093760f1b53e3394bc602f197d1db4880cb09f9
Loragert <uneas10121990@gmail.com>
2026-05-10 14:48:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request=request,
+        name="index.html"
     )
+    
